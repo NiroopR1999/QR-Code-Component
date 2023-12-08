e2610d9fce78e4f6859587d0cd725cf5a1ae2e30
Niroop R <niroopparker@gmail.com>
2023-12-09 00:49:55 +0530

    finished the qr code component

--- qr-code/src/App.jsx
+++ qr-code/src/App.jsx
@@ -1,8 +1,22 @@
-
+import "./App.scss";
+import qrcode from "./assets/images/image-qr-code.png";
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="qr-code-container">
+      <div className="qr-code-wrapper">
+        <img src={qrcode} className="qrcode-image"></img>
+        <div>
+          <span className="title">
+            Improve your front-end skills by building projects
+          </span>
+          <p className="body">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
